3b1439efcc15cef88b3cfa12bb45d2104ae60b92
julietachapur <juli.chapur@hotmail.com>
2022-10-06 23:43:13 -0300

    Empezando punto 2

---
+++ src/TP5/PanelAgregar.java
@@ -0,0 +1,50 @@
+package TP5;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PanelAgregar extends JPanel {
+    private DefaultListModel<Pelicula> listModel;
+	private static final long serialVersionUID = 1L;
+	private JButton btnAceptar;
+	private JLabel lblIdAutomatico;
+	private JTextField textFieldNombre;
+	
+	public PanelAgregar() {
+		setLayout(null);
+		
+		JLabel lblID = new JLabel("ID");
+		lblID.setBounds(90, 63, 46, 14);
+		add(lblID);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(90, 98, 46, 14);
+		add(lblNombre);
+		
+		JLabel lblGenero = new JLabel("Genero");
+		lblGenero.setBounds(90, 135, 46, 14);
+		add(lblGenero);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(201, 95, 181, 20);
+		add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		JLabel lblIdAutomatico = new JLabel("ID Automático");
+		lblIdAutomatico.setBounds(200, 63, 181, 14);
+		add(lblIdAutomatico);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(92, 179, 187, 23);
+		add(btnAceptar);
+		
+	}
+	public void setDefaultListModel(DefaultListModel<Pelicula> listModelRecibido) 
+	{
+		this.listModel = listModelRecibido;
+	}
+}
--- src/TP5/VentanaPrincipal.java
+++ src/TP5/VentanaPrincipal.java
@@ -3,27 +3,42 @@ package TP5;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
     private JPanel contentPanel;
+    private static DefaultListModel<Pelicula> listModel;
 
     public VentanaPrincipal(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
         contentPanel = new JPanel();
-        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPanel.setLayout(new BorderLayout(0,0));
-        setContentPane(contentPanel);
-        contentPanel.setLayout(null);
 
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         JMenu menu = new JMenu("Películas");
         menuBar.add(menu);
         JMenuItem menuItemAgregar = new JMenuItem("Agregar");
+        menuItemAgregar.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		contentPanel.removeAll();
+        		PanelAgregar panel = new PanelAgregar();
+        		panel.setDefaultListModel(listModel);
+        		contentPanel.add(panel);
+        		contentPanel.repaint();
+        		contentPanel.revalidate();
+        	}
+        });
         menu.add(menuItemAgregar);
         JMenuItem menuItemListar = new JMenuItem("Listar");
         menu.add(menuItemListar);
+        contentPanel = new JPanel();
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPanel.setLayout(new BorderLayout(0,0));
+        setContentPane(contentPanel);
+        contentPanel.setLayout(null);
+        
 
 
 
